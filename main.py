b52b2214b5d380dd9e77ee2f1bfaa3de01c869f2
Akash-Sardar <lordakashjoy@gmail.com>
2024-05-06 22:15:31 -0400

    Data Validation Stage Completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from NLPTextSummarizer.pipeline.stage_01_data_ingestion import *
+from NLPTextSummarizer.pipeline.stage_02_data_validation import *
 from NLPTextSummarizer.logging import logger
 
 
@@ -8,7 +9,19 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx================================x\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx================================x\n\n")
 except Exception as e:
     logger.exception(e)
     raise e
