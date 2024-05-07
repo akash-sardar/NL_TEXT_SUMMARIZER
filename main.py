37138036b46774f24daf06b03c14473f158762c9
Akash-Sardar <lordakashjoy@gmail.com>
2024-05-06 23:55:02 -0400

    Data Transformation  Layer added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from NLPTextSummarizer.pipeline.stage_01_data_ingestion import *
 from NLPTextSummarizer.pipeline.stage_02_data_validation import *
+from NLPTextSummarizer.pipeline.stage_03_data_transformation import *
 from NLPTextSummarizer.logging import logger
 
 
@@ -21,6 +22,18 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx================================x\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data TransformationStage"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx================================x\n\n")
 except Exception as e:
     logger.exception(e)
